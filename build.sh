07c5bc678875adb41d1c0394a8c3e9b5a1d62928
peace <121939750+SomeEmptyBox@users.noreply.github.com>
2025-05-19 11:58:46 +0530

    add wild kernel patches and some fixes

--- build.sh
+++ build.sh
@@ -6,12 +6,9 @@ function sync() {
     repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
     /opt/crave/resync.sh
     echo "===== apply patches ====="
-    cd packages/services/Telephony
-    curl https://github.com/2by2-Project/packages_services_Telephony/commit/23ef951cffca4f02d7435c934fab94bf4fb7030e.patch | git apply
-    cd -
-    cd vendor/qcom/opensource/vibrator
-    curl https://github.com/moto-sm7550-devs/android_vendor_qcom_opensource_vibrator/commit/15b802a88b5601ed1ae31bab33c83be6f4d01a02.patch | git apply
-    cd -
+    rm -rf packages/services/Telephony vendor/qcom/opensource/vibrator
+    git clone --depth 1 --branch lineage-22.2 https://github.com/SomeEmptyBox/android_packages_services_Telephony packages/services/Telephony
+    git clone --depth 1 --branch lineage-22.2 https://github.com/moto-sm7550-devs/android_vendor_qcom_opensource_vibrator vendor/qcom/opensource/vibrator
     echo "===== completed ====="
     echo " "
 }
@@ -22,12 +19,12 @@ function clone() {
     rm -rf {device,vendor,kernel,hardware}/motorola vendor/evolution-priv/keys
     git clone --depth 1 --branch lineage-22.2 https://github.com/SomeEmptyBox/android_device_motorola_eqe device/motorola/eqe
     git clone --depth 1 --branch lineage-22.2 https://github.com/SomeEmptyBox/android_hardware_motorola hardware/motorola
-    git clone --depth 1 https://github.com/SomeEmptyBox/vendor_evolution-priv_keys vendor/evolution-priv/keys
+    git clone --depth 1 https://github.com/SomeEmptyBox/android_vendor_evolution-priv_keys vendor/evolution-priv/keys
     echo "===== clone vendor ====="
     git clone --depth 1 --branch lineage-22.2 https://gitlab.com/moto-sm7550/proprietary_vendor_motorola_eqe vendor/motorola/eqe
     git clone --depth 1 https://gitlab.com/moto-sm7550/proprietary_vendor_motorola_eqe-motcamera vendor/motorola/eqe-motcamera
     echo "===== clone kernel ====="
-    git clone --depth 1 https://github.com/SomeEmptyBox/android_kernel_motorola_sm7550 kernel/motorola/sm7550
+    git clone --depth 1 https://github.com/moto-sm7550-devs/android_kernel_motorola_sm7550 kernel/motorola/sm7550
     git clone --depth 1 https://github.com/moto-sm7550-devs/android_kernel_motorola_sm7550-modules kernel/motorola/sm7550-modules
     git clone --depth 1 https://github.com/moto-sm7550-devs/android_kernel_motorola_sm7550-devicetrees kernel/motorola/sm7550-devicetrees
     echo "===== completed ====="
@@ -36,14 +33,70 @@ function clone() {
 
 function root() {
     echo " "
-    echo "===== integrate KernelSU Next + SuSFS ====="
+    echo "===== integrate KernelSU Next with SUSFS and Wild Kernel patches ====="
     cd kernel/motorola/sm7550
-    curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next-susfs/kernel/setup.sh" | bash -s next-susfs
+
+    # KernelSU Next SUSFS
+    curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s next-susfs-dev
+
+    # SUSFS patches for Kernel
     git clone -b gki-android13-5.15 https://gitlab.com/simonpunk/susfs4ksu susfs
     cp ./susfs/kernel_patches/50_add_susfs_in_gki-android13-5.15.patch .
     cp ./susfs/kernel_patches/fs/* fs
     cp ./susfs/kernel_patches/include/linux/* include/linux
-    patch -p1 < 50_add_susfs_in_gki-android13-5.15.patch
+    patch -p1 --fuzz=3 <50_add_susfs_in_gki-android13-5.15.patch
+
+    # Wild kernel patches
+    curl "https://raw.githubusercontent.com/WildKernels/kernel_patches/refs/heads/main/next/syscall_hooks.patch" | patch -p1 --fuzz=3
+    curl "https://raw.githubusercontent.com/WildKernels/kernel_patches/refs/heads/main/69_hide_stuff.patch" | patch -p1 --fuzz=3
+
+    # SUSFS backport patch
+    curl "https://raw.githubusercontent.com/SomeEmptyBox/android_eqe/refs/heads/main/susfs_backport.patch" | patch -p1 --fuzz=3
+
+    # Add configuration settings to gki_defconfig
+    echo "CONFIG_KSU=y" >> ./arch/arm64/configs/gki_defconfig
+    echo "CONFIG_KSU_WITH_KPROBES=n" >> ./arch/arm64/configs/gki_defconfig
+
+    # Add tmpfs config setting
+    echo "CONFIG_TMPFS_XATTR=y" >> ./arch/arm64/configs/gki_defconfig
+    echo "CONFIG_TMPFS_POSIX_ACL=y" >> ./arch/arm64/configs/gki_defconfig
+
+    # Add additional config setting
+    echo "CONFIG_IP_NF_TARGET_TTL=y" >> ./arch/arm64/configs/gki_defconfig
+    echo "CONFIG_IP6_NF_TARGET_HL=y" >> ./arch/arm64/configs/gki_defconfig
+    echo "CONFIG_IP6_NF_MATCH_HL=y" >> ./arch/arm64/configs/gki_defconfig
+
+    # Add BBR Config
+    echo "CONFIG_TCP_CONG_ADVANCED=y" >> ./arch/arm64/configs/gki_defconfig
+    echo "CONFIG_TCP_CONG_BBR=y" >> ./arch/arm64/configs/gki_defconfig
+    echo "CONFIG_NET_SCH_FQ=y" >> ./arch/arm64/configs/gki_defconfig
+    echo "CONFIG_TCP_CONG_BIC=n" >> ./arch/arm64/configs/gki_defconfig
+    echo "CONFIG_TCP_CONG_WESTWOOD=n" >> ./arch/arm64/configs/gki_defconfig
+    echo "CONFIG_TCP_CONG_HTCP=n" >> ./arch/arm64/configs/gki_defconfig
+
+    # Add SUSFS configuration settings
+    echo "CONFIG_KSU_SUSFS=y" >> ./arch/arm64/configs/gki_defconfig
+    echo "CONFIG_KSU_SUSFS_HAS_MAGIC_MOUNT=y" >> ./arch/arm64/configs/gki_defconfig
+    echo "CONFIG_KSU_SUSFS_SUS_PATH=y" >> ./arch/arm64/configs/gki_defconfig
+    echo "CONFIG_KSU_SUSFS_SUS_MOUNT=y" >> ./arch/arm64/configs/gki_defconfig
+    echo "CONFIG_KSU_SUSFS_AUTO_ADD_SUS_KSU_DEFAULT_MOUNT=y" >> ./arch/arm64/configs/gki_defconfig
+    echo "CONFIG_KSU_SUSFS_AUTO_ADD_SUS_BIND_MOUNT=y" >> ./arch/arm64/configs/gki_defconfig
+    echo "CONFIG_KSU_SUSFS_SUS_KSTAT=y" >> ./arch/arm64/configs/gki_defconfig
+    echo "CONFIG_KSU_SUSFS_SUS_OVERLAYFS=n" >> ./arch/arm64/configs/gki_defconfig
+    echo "CONFIG_KSU_SUSFS_TRY_UMOUNT=y" >> ./arch/arm64/configs/gki_defconfig
+    echo "CONFIG_KSU_SUSFS_AUTO_ADD_TRY_UMOUNT_FOR_BIND_MOUNT=y" >> ./arch/arm64/configs/gki_defconfig
+    echo "CONFIG_KSU_SUSFS_SPOOF_UNAME=y" >> ./arch/arm64/configs/gki_defconfig
+    echo "CONFIG_KSU_SUSFS_ENABLE_LOG=y" >> ./arch/arm64/configs/gki_defconfig
+    echo "CONFIG_KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS=y" >> ./arch/arm64/configs/gki_defconfig
+    echo "CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG=y" >> ./arch/arm64/configs/gki_defconfig
+    echo "CONFIG_KSU_SUSFS_OPEN_REDIRECT=y" >> ./arch/arm64/configs/gki_defconfig
+    echo "CONFIG_KSU_SUSFS_SUS_SU=n" >> ./arch/arm64/configs/gki_defconfig
+
+    # Some random fixes
+    sed -i 's/check_defconfig//' ./build.config.gki
+    sed -i 's/-dirty/-peace/g' ./common/scripts/setlocalversion
+    sed -i '2435s/timestamp/*timestamp/g' ./include/uapi/linux/videodev2.h
+
     cd -
     echo "===== completed ====="
     echo " "
