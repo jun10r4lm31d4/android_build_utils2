4104e547d67b43a3ce62e3e0b4f90fc2b39ef4ad
peace <121939750+SomeEmptyBox@users.noreply.github.com>
2025-05-28 11:30:34 +0530

    scripts: fix spacing

--- scripts/root.sh
+++ scripts/root.sh
@@ -71,14 +71,14 @@ fi
 
 echo "Adding configuration settings to gki_defconfig..."
 curl -fLSs "${wild_kernel}" |
-  grep 'CONFIG_KSU' |
-  if [[ "${ksu_variant}" == "ksu" ]]; then
-    grep -v -E 'SUS_SU=n|KPROBES_HOOK=n'
-  else
-    grep -v 'SUS_SU=y'
-  fi |
-  awk '{print $2}' |
-  sed 's/"//g' >> ./arch/arm64/configs/gki_defconfig
+    grep 'CONFIG_KSU' |
+    if [[ "${ksu_variant}" == "ksu" ]]; then
+        grep -v -E 'SUS_SU=n|KPROBES_HOOK=n'
+    else
+        grep -v 'SUS_SU=y'
+    fi |
+    awk '{print $2}' |
+    sed 's/"//g' >> ./arch/arm64/configs/gki_defconfig
 
 sed -i 's/check_defconfig//' ./build.config.gki
 
