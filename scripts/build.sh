a421fd49283a70cc13ba98b69b0aa3b28c248e02
peace <121939750+SomeEmptyBox@users.noreply.github.com>
2025-05-30 11:53:07 +0530

    scripts: preserve vendor

--- scripts/build.sh
+++ scripts/build.sh
@@ -31,7 +31,7 @@ handle_error() {
 
 cleanup() {
     echo "Cleaning up..."
-    rm -rf {device,vendor,kernel,hardware}/motorola vendor/private .repo/local_manifests
+    rm -rf {device,kernel,hardware}/motorola vendor/private .repo/local_manifests
     unset GH_TOKEN
     echo "Exiting."
 }
