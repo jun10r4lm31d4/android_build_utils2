c766e8f576169a3d7932e13ad533d2eab2f40c4e
Peace <121939750+SomeEmptyBox@users.noreply.github.com>
2025-06-28 01:18:08 +0530

    scripts: update kernel patch path

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -12,10 +12,10 @@ fetch() {
     fi
 }
 
-fetch "patches/susfs_ksu.patch"         "https://gitlab.com/simonpunk/susfs4ksu/-/raw/gki-android13-5.15/kernel_patches/KernelSU/10_enable_susfs_for_ksu.patch?ref_type=heads"
-fetch "patches/susfs_kernel.patch"      "https://gitlab.com/simonpunk/susfs4ksu/-/raw/gki-android13-5.15/kernel_patches/50_add_susfs_in_gki-android13-5.15.patch?ref_type=heads"
-fetch "patches/hide_stuff.patch"        "https://raw.githubusercontent.com/WildKernels/kernel_patches/refs/heads/main/69_hide_stuff.patch"
-fetch "patches/syscall_hooks.patch"     "https://raw.githubusercontent.com/WildKernels/kernel_patches/refs/heads/main/next/syscall_hooks.patch"
+fetch "patches/kernel/susfs_ksu.patch"         "https://gitlab.com/simonpunk/susfs4ksu/-/raw/gki-android13-5.15/kernel_patches/KernelSU/10_enable_susfs_for_ksu.patch?ref_type=heads"
+fetch "patches/kernel/susfs_kernel.patch"      "https://gitlab.com/simonpunk/susfs4ksu/-/raw/gki-android13-5.15/kernel_patches/50_add_susfs_in_gki-android13-5.15.patch?ref_type=heads"
+fetch "patches/kernel/hide_stuff.patch"        "https://raw.githubusercontent.com/WildKernels/kernel_patches/refs/heads/main/69_hide_stuff.patch"
+fetch "patches/kernel/syscall_hooks.patch"     "https://raw.githubusercontent.com/WildKernels/kernel_patches/refs/heads/main/next/syscall_hooks.patch"
 
 fetch "scripts/resync.sh"               "https://raw.githubusercontent.com/accupara/docker-images/refs/heads/master/aosp/common/resync.sh"
 fetch "scripts/upload.sh"               "https://raw.githubusercontent.com/Drenzzz/script-upload/refs/heads/master/upload.sh"
